b9ed77ffe8711fdb2c167ac1cb8bb6858dfe0682
Benjamin Tse <benjamin.l.tse@gmail.com>
2018-05-12 21:52:15 +1000

    Add basic logging

--- SerilogSandpit/Controllers/ValuesController.cs
+++ SerilogSandpit/Controllers/ValuesController.cs
@@ -3,16 +3,26 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Serilog;
 
 namespace SerilogSandpit.Controllers
 {
     [Route("api/[controller]")]
     public class ValuesController : Controller
     {
+        private readonly ILogger<ValuesController> _logger;
+
+        public ValuesController(ILogger<ValuesController> logger)
+        {
+           this._logger = logger;
+        }
+            
         // GET api/values
         [HttpGet]
         public IEnumerable<string> Get()
         {
+            _logger.LogInformation("Get");
             return new string[] { "value1", "value2" };
         }
 
@@ -20,6 +30,7 @@ namespace SerilogSandpit.Controllers
         [HttpGet("{id}")]
         public string Get(int id)
         {
+            _logger.LogInformation("Get {id}", id);
             return "value";
         }
 
@@ -27,18 +38,21 @@ namespace SerilogSandpit.Controllers
         [HttpPost]
         public void Post([FromBody]string value)
         {
+            _logger.LogInformation("Post {value}", value);
         }
 
         // PUT api/values/5
         [HttpPut("{id}")]
         public void Put(int id, [FromBody]string value)
         {
+            _logger.LogInformation("Put {value}", value);
         }
 
         // DELETE api/values/5
         [HttpDelete("{id}")]
         public void Delete(int id)
         {
+            _logger.LogInformation("Delete {id}", id);
         }
     }
 }
